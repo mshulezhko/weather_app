b2472e37f451ecee91d17e214f364e020b85990f
mshulezhko <119418285+mshulezhko@users.noreply.github.com>
2023-05-17 17:18:08 +0200

    Showing the current date

--- src/app.js
+++ src/app.js
@@ -3,6 +3,26 @@ let cityName = 'Kiev'
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
 console.log(apiUrl)
 
+function formatDate(timestamp) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    let date = new Date(timestamp)
+    let hours = date.getHours()
+
+    if (hours < 10) {
+        hours = `0${date.getHours()}`
+    }
+
+    let minutes = date.getMinutes()
+
+    if (minutes < 10) {
+        minutes = `0${date.getMinutes()}`
+    }
+
+    let day = date.getDay()
+
+    return `${days[day]} ${hours}:${minutes}`
+}
+
 function showTemperature(response) {
     cityNameElement = document.querySelector('#cityName')
     cityNameElement.innerHTML = response.data.name
@@ -14,8 +34,10 @@ function showTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity
     windElement = document.querySelector('#wind')
     windElement.innerHTML = response.data.wind.speed
+    dateElement = document.querySelector('#date')
+    dateElement.innerHTML = formatDate(response.data.dt * 1000)
     console.log(response.data)
-    console.log(response.data.wind.speed)
+    console.log(formatDate(response.data.dt * 1000))
 }
 
 axios.get(apiUrl).then(showTemperature)
