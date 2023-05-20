6ed8fddfb91a3a35d537e25658a69b56308c86b3
mshulezhko <119418285+mshulezhko@users.noreply.github.com>
2023-05-20 20:09:27 +0200

    added functional for showing forecast for five days

--- src/app.js
+++ src/app.js
@@ -18,6 +18,43 @@ function formatDate(timestamp) {
     return `${days[day]} ${hours}:${minutes}`
 }
 
+function showForecast(response) {
+    let forecast = response.data.daily
+    let forecastElement = document.querySelector('#forecast')
+    let forecastHtml = ''
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+    forecast.forEach(function (day, index) {
+        if (index < 6) {
+            let date = new Date(day.dt * 1000)
+            let newDay = date.getDay()
+
+            forecastHtml = forecastHtml + `<div class="col">
+              <div class="weather-forecast-date">${days[newDay]}</div>
+              <div class="weather-forecast-img">
+                <img
+                  src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
+                  alt="${day.weather[0].description}"
+                />
+              </div>
+              <div class="weather-forecast-temp">
+                <span class="weather-forecast-temp-max">${Math.round(day.temp.max)}</span>
+                <span class="weather-forecast-temp-min">${Math.round(day.temp.min)}</span>
+              </div>
+            </div>`
+        }
+    })
+
+    forecastElement.innerHTML = forecastHtml
+
+}
+
+function getForecastData(coordinates) {
+    let apiKey = 'b95f179627c8dd37f41e1be6e3250e19'
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`
+    axios.get(apiUrl).then(showForecast)
+}
+
 function showTemperature(response) {
     cityNameElement = document.querySelector('#cityName')
     cityNameElement.innerHTML = response.data.name
@@ -36,6 +73,8 @@ function showTemperature(response) {
     let urlIcon = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     iconElement.setAttribute('src', urlIcon)
     iconElement.setAttribute('alt', response.data.weather[0].description)
+
+    getForecastData(response.data.coord)
 }
 
 
